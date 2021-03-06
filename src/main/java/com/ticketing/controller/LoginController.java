96e461ebeed231487b54b75d306575526692cb9e
memintan <memintan@gmail.com>
2021-03-06 01:43:46 -0600

    Jd Ticketing Project - Rest

--- src/main/java/com/ticketing/controller/LoginController.java
+++ src/main/java/com/ticketing/controller/LoginController.java
@@ -9,17 +9,17 @@ import com.ticketing.exception.TicketingProjectException;
 import com.ticketing.mapper.MapperUtil;
 import com.ticketing.service.UserService;
 import com.ticketing.util.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
-
+@Tag(name = "Authentication Controller",description = "Authenticate API")
 public class LoginController {
 
 	private AuthenticationManager authenticationManager;
@@ -38,8 +38,8 @@ public class LoginController {
 
 	@PostMapping("/authenticate")
 	@DefaultExceptionMessage(defaultMessage = "Bad Credentials")
-
-	public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest) throws TicketingProjectException {
+	@Operation(summary = "Login to application")
+	public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest) throws TicketingProjectException, AccessDeniedException {
 
 		String password = authenticationRequest.getPassword();
 		String username = authenticationRequest.getUsername();
