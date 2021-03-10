2cf4c74e27430e3cb602adff83e998a01654fb77
memintan <memintan@gmail.com>
2021-03-09 19:06:51 -0600

    Jd Ticketing Project - Unit Testing

--- src/test/java/com/ticketing/controller/ProjectControllerTest.java
+++ src/test/java/com/ticketing/controller/ProjectControllerTest.java
@@ -5,15 +5,25 @@ import com.ticketing.dto.RoleDTO;
 import com.ticketing.dto.UserDTO;
 import com.ticketing.enums.Gender;
 import com.ticketing.enums.Status;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ticketing.dto.ProjectDTO;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,7 +32,7 @@ class ProjectControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJpZCI6MSwiZXhwIjoxNjE1MDkzODQ2LCJpYXQiOjE2MTUwNTc4NDYsInVzZXJuYW1lIjoiYWRtaW5AYWRtaW4uY29tIn0.sS1OXeMkTv1FEWHTyDsHh5l1QSpS9t5bkTgV8ggcq5Y";
+    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJpZCI6MSwiZXhwIjoxNjE1MzYzOTg1LCJpYXQiOjE2MTUzMjc5ODUsInVzZXJuYW1lIjoiYWRtaW5AYWRtaW4uY29tIn0.MqKhwZ547h8Cf00J6YdiVTbJAVCxvb6BlGwAnoXhcsU";
 
     static UserDTO userDTO;
     static ProjectDTO projectDTO;
@@ -32,8 +42,8 @@ class ProjectControllerTest {
 
         userDTO = UserDTO.builder()
                 .id(2L)
-                .firstName("Mike")
-                .lastName("Smith")
+                .firstName("mike")
+                .lastName("smith")
                 .userName("ozzy@cybertekschool.com")
                 .passWord("abc123")
                 .confirmPassword("abc123")
@@ -42,7 +52,7 @@ class ProjectControllerTest {
                 .build();
 
         projectDTO = ProjectDTO.builder()
-                .projectCode("Api1")
+                .projectCode("Api2")
                 .projectName("Api")
                 .assignedManager(userDTO)
                 .startDate(LocalDate.now())
@@ -52,9 +62,62 @@ class ProjectControllerTest {
                 .completeTaskCounts(0)
                 .unfinishedTaskCounts(0)
                 .build();
+    }
+
+    @Test
+    public void givenToken_deleteProject() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+        .delete("/api/v1/project" + projectDTO.getProjectCode())
+        .header("Authorization", token)
+        .content(toJsonString(projectDTO))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 
+    @Test
+    public void givenNoToken_whenGetSecureRequest() throws Exception {
 
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/project/Api1"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void givenToken_getAllProjects() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/project")
+                .header("Authorization",token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].projectCode").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].assignedManager.userName").isNotEmpty());
 
     }
+
+    @Test
+    public void givenToken_createProject() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/project")
+                .header("Authorization",token)
+                .content(toJsonString(projectDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.projectCode").isNotEmpty());
+    }
+
+
+    protected static String toJsonString(final Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
